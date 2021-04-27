ac0d813afeff12d15c48734be449d4c95eae823a
Sambhavi PD <sambhavi@golittlebig.com>
2021-04-27 20:56:20 +0530

    Removed Classify button and default image

--- flowers_app.py
+++ flowers_app.py
@@ -22,32 +22,25 @@ st.sidebar.text('url = "http://download.tensorflow.org/example_images/flower_pho
 
 st.sidebar.title('Future work')
 
-st.subheader('Step 1: Upload a flower image')
-uploaded_file = st.file_uploader(label="The models are trained to classify sunflower, rose, dandelion, tulip and daisy only.", type=['png', 'jpg', 'jpeg'], accept_multiple_files=False, key="None", help="Only .png, .jpg, and .jpeg files are supported")
-
-st.subheader('Step 2: Click on Classify button below')
-st.button('Classify')
+st.subheader('Upload a flower image')
+uploaded_file = st.file_uploader(label="The models are trained to classify sunflower, rose, dandelion, tulip and daisy only.", 	type=['png', 'jpg', 'jpeg'], accept_multiple_files=False, key="None", help="Only .png, .jpg, and .jpeg files are supported")
 
 classifyapi_url = "http://139.59.13.253:8008/predict"
 
-if uploaded_file is None:
-	st.subheader('Please upload an image first and then click Classify!')
-else:
-	uploaded_file="https://github.com/SambhaviPD/flower-classification/tree/main/images/rose.jpg"
-	
-m = MultipartEncoder(
-	fields={'input_image': ('filename', uploaded_file, 'image/jpeg')}
-)
+if uploaded_file is not None:
+	m = MultipartEncoder(
+		fields={'input_image': ('filename', uploaded_file, 'image/jpeg')}
+	)
 
-response = requests.post(classifyapi_url, 
-				data=m,
-				headers={
-					'Content-Type': m.content_type
-				})
-if response is None:
-	st.subheader('Classify API is down. Please try later!')
-else:
-	output = response.json()
+	response = requests.post(classifyapi_url, 
+					data=m,
+					headers={
+						'Content-Type': m.content_type
+					})
+	if response is None:
+		st.subheader('Classify API is down. Please try later!')
+	else:
+		output = response.json()
 
-	st.subheader('Output: Here''s what the model classifies your input as:')					
-	st.markdown(output['Prediction'])
+		st.subheader('Output: Here''s what the model classifies your input as:')					
+		st.markdown(output['Prediction'])
