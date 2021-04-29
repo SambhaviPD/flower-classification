676fd8acb741df148d111353622670551febeb76
Sambhavi PD <sambhavi@golittlebig.com>
2021-04-29 16:46:27 +0530

    Changed the path of image

--- flowers_app.py
+++ flowers_app.py
@@ -42,7 +42,7 @@ expander.write('A total of 3670 images is present, approximately 700-800 images 
 expander.write('50 epochs were run for training the dataset. Weights and Biases was used for experiment tracking.')
 expander.write('80% of images was used for training, 10% for validation and 5% for testing. A training accuracy of 92% was reached.')
 expander.write('This is how the confusion matrix looks like:')
-image_url = 'https://github.com/SambhaviPD/flower-classification/tree/main/images/confusion-matrix.jpg'
+image_url = 'https://ibb.co/mzYm1Kc'
 expander.image(image_url, width=500)
 expander.write('FastAPI was used to invoke the actual prediction of uploaded flower used the weights trained as explained above. That piece of code is deployed in another cloud server.')
 
